54ccfca9a1290eec3bebb8ad8f0d0dcab5dcd6ff
Milovidov <andryshka.yeah@gmail.com>
2022-07-11 21:36:20 +0300

    hw4

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -5,54 +5,46 @@ import java.util.Arrays;
 public class ArrayTheme {
     public static void main(String[] args) {
         System.out.println("1. Реверс значений массива");
-        int[] numbers = {
-                1,
-                2,
-                3,
-                4,
-                5,
-                6,
-                7
-        };
-        for (int number: numbers) {
+        int[] intArr = {1, 2, 3, 4, 5, 6, 7};
+        for (int number : intArr) {
             System.out.print(number + " ");
         }
-        int len1 = numbers.length;
+        int len1 = intArr.length;
         System.out.println("\n");
         int[] numbers1 = new int[len1];
         for (int i = 0; i < len1; i++) {
-            numbers1[i] = numbers[(len1 - 1 - i)];
+            numbers1[i] = intArr[len1 - 1 - i];
             System.out.print(numbers1[i] + " ");
         }
-        System.out.println("\n");
 
-        System.out.println("2. Вывод произведения элементов массива");
+        System.out.println("\n\n2. Вывод произведения элементов массива");
         int[] numbers2 = new int[10];
         int len2 = numbers2.length;
         for (int i = 0; i < len2; i++) {
             numbers2[i] = i;
         }
-        int sum = 0;
+        int sum = 1;
         for (int i = 1; i < len2 - 1; i++) {
-            sum += numbers2[i];
-            String answer = numbers2[i] < len2 - 2 ? String.valueOf(numbers2[i]) + "*" : String.valueOf(numbers2[i]);
+            sum = sum * numbers2[i];
+            String answer = numbers2[i] < len2 - 2 ? (String.valueOf(numbers2[i]) + " * ") :
+                    (String.valueOf(numbers2[i]) + " = " + sum + "\n");
             System.out.print(answer);
+
         }
-        System.out.println("=" + sum);
         for (int i = 0; i < len2; i++) {
-            String tail = numbers2[i] == numbers2[0] || numbers2[i] == numbers2[len2 - 1] ? "Индекс " + i + " соответствует числу " + String.valueOf(numbers2[i]) + " " : "";
+            String tail = numbers2[i] == numbers2[0] || numbers2[i] == numbers2[len2 - 1] ? "\nИндекс " + i +
+                    " соответствует числу " + String.valueOf(numbers2[i]) + " " : "";
             System.out.print(tail);
         }
-        System.out.println("\n");
 
-        System.out.println("3. Удаление элементов массива");
+        System.out.println("\n\n3. Удаление элементов массива");
         double[] numbers3 = new double[15];
         int len3 = numbers3.length;
         for (int i = 0; i < len3; i++) {
             numbers3[i] = Math.random();
         }
         System.out.println("Исходный массив:");
-        for (double number: numbers3) {
+        for (double number : numbers3) {
             System.out.println(number + " ");
         }
         double[] newnumbers3 = Arrays.copyOf(numbers3, len3);
@@ -89,7 +81,7 @@ public class ArrayTheme {
         for (int i = 0; i < len5; i++) {
             do {
                 unic = true;
-                newnumber = (int)(Math.random() * 40 + 1) + 60;
+                newnumber = (int) (Math.random() * 40 + 1) + 60;
                 for (int k = 0; k < len5; k++) {
                     if (newnumber == numbers5[k]) {
                         unic = false;
@@ -99,7 +91,7 @@ public class ArrayTheme {
             } while (!unic);
             numbers5[i] = newnumber;
         }
-        for (int number: numbers5) {
+        for (int number : numbers5) {
             System.out.print(number + " ");
         }
     }
