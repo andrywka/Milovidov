f2e9a367c47fd2c0e8b335575152877dc5becd85
Milovidov <andryshka.yeah@gmail.com>
2022-08-18 18:49:52 +0300

    guess hw4

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -6,8 +6,6 @@ public class GuessNumber {
     private Player player1;
     private Player player2;
 
-    private int attempt;
-
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
